bfcd76799f6b64650cb47f0961c7e9df73345317
sharonBenish <sbenishwilliams@gmail.com>
2025-01-30 05:17:33 -0600

    first commit

---
+++ src/views/auth/Login.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <p class="text-3xl font-bold underline">Helllo</p>
+    </div>
+</template>
+
+<style lang="css">
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {
+        colors: {
+            'light-grey': 'hsl(0, 0%, 98%)',
+            'purple': 'hsl(252, 100%, 62%)',
+            'purple-hover': 'hsl(252, 100%, 84%)',
+            'light-purple': 'hsl(252, 100%, 96%)',
+            'dark-grey': 'hsl(0, 0%, 20%)',
+            'grey': 'hsl( 0, 0%, 45%)',
+            'borders': 'hsl(0, 0%, 85%)',
+            'red': 'hsl(0, 100%, 61%)'
+        }
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
