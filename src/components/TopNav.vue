274a504fbbb4477e656d7c29dfcf1624eba3fcbd
sharonBenish <sbenishwilliams@gmail.com>
2025-07-19 06:29:44 -0500

    feat: route to login

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,12 +1,18 @@
 <script lang="ts" setup>
+import { useRouter } from 'vue-router';
+
 const props = defineProps<{
   activeTab: string
 }>()
-
+const router = useRouter();
 const emit = defineEmits(['tabChanged'])
 
 function changeTab (tab: string){
     emit('tabChanged', tab)
+}
+
+function goToLogin() {
+    router.push({ name: 'login' });
 }
 </script>
 
@@ -29,7 +35,7 @@ function changeTab (tab: string){
             <RouterLink :to="{name: 'preview'}" class="w-full py-2.5 px-2.5 md:px-4 rounded-md border border-purple bg-white cursor-pointer text-purple font-[500] flex gap-2 items-center hover:bg-light-purple">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="fill-purple md:hidden" viewBox="0 0 20 20"><path d="M19.61 9.62c-.03-.064-.714-1.583-2.225-3.095-2.023-2.02-4.572-3.088-7.385-3.088-2.812 0-5.362 1.068-7.382 3.088C1.106 8.037.422 9.556.391 9.62a.944.944 0 0 0 0 .761c.029.064.713 1.583 2.226 3.095 2.021 2.02 4.57 3.086 7.383 3.086 2.813 0 5.362-1.067 7.381-3.086 1.513-1.512 2.197-3.03 2.226-3.095a.946.946 0 0 0 .003-.761Zm-3.599 2.578c-1.677 1.651-3.7 2.49-6.01 2.49-2.313 0-4.334-.839-6.01-2.491A10.185 10.185 0 0 1 2.307 10a10.192 10.192 0 0 1 1.686-2.196C5.667 6.15 7.688 5.312 10 5.312s4.333.839 6.009 2.492c.659.652 1.226 1.39 1.685 2.196a10.19 10.19 0 0 1-1.685 2.197h.002Zm-6.01-5.636a3.438 3.438 0 1 0 0 6.876 3.438 3.438 0 0 0 0-6.876Zm0 5A1.562 1.562 0 1 1 10 8.438a1.562 1.562 0 0 1 0 3.124Z"/></svg><span class="max-md:hidden font-[500]">Preview</span>
             </RouterLink>
-            <button>
+            <button @click="goToLogin">
                 <svg width="5" height="20" xmlns="http://www.w3.org/2000/svg"><g fill="#828FA3" fill-rule="evenodd"><circle cx="2.308" cy="2.308" r="2.308"/><circle cx="2.308" cy="10" r="2.308"/><circle cx="2.308" cy="17.692" r="2.308"/></g></svg>
             </button>
         </div>
